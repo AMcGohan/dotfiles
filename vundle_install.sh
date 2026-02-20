0e618538ec29c22867164a724b7bebbf3094a11a
Aidan McGohan <aidanmcgohan@gmail.com>
2026-02-20 13:05:50 -0500

    fixed ordering for plugin install

--- vundle_install.sh
+++ vundle_install.sh
@@ -9,14 +9,16 @@ VundleDIR=~/.vim/bundle/Vundle.vim
 if [ -d $VundleDIR ]; then
        echo "Vundle already exists, skipping installation."
 else
-	echo "Installing Vundle..."
-	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+        echo "Installing Vundle..."
+        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
 
 echo "Installing vim plugin dependencies..."
 
+vim +PluginInstall +qall
 # https://github.com/ycm-core/YouCompleteMe?tab=readme-ov-file#linux-64-bit
 sudo apt install build-essential cmake vim-nox python3-dev -y
 sudo apt install mono-complete golang nodejs default-jdk npm -y
 
 python3 ~/.vim/bundle/youcompleteme/install.py --all
+
