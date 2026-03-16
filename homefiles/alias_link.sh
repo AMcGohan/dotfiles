b20d2c069e3e5bbf99c3a5a61c78f15d8c5c71e5
Aidan McGohan <aidanmcgohan@gmail.com>
2026-03-16 13:01:50 -0400

    changed script to make a symbolic link instead of copying file

--- homefiles/alias_link.sh
+++ homefiles/alias_link.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Copies bash aliases to users home directory
-cp ./.bash_aliases $HOME/
+sh_path=$(pwd)
+
+# creates a symbolic link file in users home directory that points to `.bash_aliases` in this directory
+
+ln -s $sh_path/.bash_aliases ~/.bash_aliases
 
 echo "Reload your .bash_aliases file for changes to take effect e.g. source ~/.bash_aliases"
